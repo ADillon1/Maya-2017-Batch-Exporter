3d08e8123073902e0e89fe3092e6ea4d821f0cb6
Andrew Dillon <a.dillon@digipen.edu>
2017-11-06 12:50:22 -0800

    Added options making freezing the transforms and moving them to the origin optional.

--- batch-exporter.py
+++ batch-exporter.py
@@ -15,36 +15,44 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 328)
+        Dialog.resize(400, 371)
         self.change_btn = QtWidgets.QPushButton(Dialog)
         self.change_btn.setGeometry(QtCore.QRect(320, 10, 75, 23))
         self.change_btn.setObjectName("change_btn")
         self.label = QtWidgets.QLabel(Dialog)
         self.label.setGeometry(QtCore.QRect(10, 10, 47, 21))
         self.label.setObjectName("label")
-        self.comboBox = QtWidgets.QComboBox(Dialog)
-        self.comboBox.setGeometry(QtCore.QRect(320, 300, 71, 22))
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
         self.textBox = QtWidgets.QLineEdit(Dialog)
         self.textBox.setEnabled(False)
         self.textBox.setGeometry(QtCore.QRect(60, 10, 251, 20))
         self.textBox.setObjectName("textBox")
         self.export_btn = QtWidgets.QPushButton(Dialog)
-        self.export_btn.setGeometry(QtCore.QRect(10, 300, 75, 23))
+        self.export_btn.setGeometry(QtCore.QRect(10, 340, 75, 23))
         self.export_btn.setObjectName("export_btn")
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(270, 300, 51, 21))
-        self.label_2.setObjectName("label_2")
         self.output = QtWidgets.QPlainTextEdit(Dialog)
         self.output.setEnabled(True)
         self.output.setGeometry(QtCore.QRect(10, 40, 381, 251))
         self.output.setReadOnly(True)
         self.output.setPlaceholderText("")
         self.output.setObjectName("output")
+        self.comboBox = QtWidgets.QComboBox(Dialog)
+        self.comboBox.setGeometry(QtCore.QRect(320, 340, 71, 22))
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(280, 340, 31, 21))
+        self.label_2.setObjectName("label_2")
+        self.checkBox = QtWidgets.QCheckBox(Dialog)
+        self.checkBox.setGeometry(QtCore.QRect(280, 300, 111, 17))
+        self.checkBox.setChecked(True)
+        self.checkBox.setObjectName("checkBox")
+        self.checkBox_2 = QtWidgets.QCheckBox(Dialog)
+        self.checkBox_2.setGeometry(QtCore.QRect(280, 320, 111, 17))
+        self.checkBox_2.setChecked(True)
+        self.checkBox_2.setObjectName("checkBox_2")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -60,6 +68,8 @@ class Ui_Dialog(object):
         self.textBox.setText(QtWidgets.QApplication.translate("Dialog", "C:", None, -1))
         self.export_btn.setText(QtWidgets.QApplication.translate("Dialog", "Export", None, -1))
         self.label_2.setText(QtWidgets.QApplication.translate("Dialog", "File Type:", None, -1))
+        self.checkBox.setText(QtWidgets.QApplication.translate("Dialog", "Freeze Transforms", None, -1))
+        self.checkBox_2.setText(QtWidgets.QApplication.translate("Dialog", "Move to Origin", None, -1))
 
 class Exporter(QtWidgets.QDialog):
     def __init__(self, parent=QtWidgets.QApplication.activeWindow()):
@@ -117,23 +127,29 @@ class Exporter(QtWidgets.QDialog):
 
         self.ui.output.clear()
         self.ui.output.setPlainText("Location: " + path + "\n")
+        
+        freeze = self.ui.checkBox.isChecked()
+        move = self.ui.checkBox2.isChecked()
 
         for selection in selected:
             text = self.ui.output.toPlainText()
             cmds.select(selection, r=True)
             
-            # store object position
-            x_value = cmds.getAttr("%s.translateX" % selection)
-            y_value = cmds.getAttr("%s.translateY" % selection)
-            z_value = cmds.getAttr("%s.translateZ" % selection)
-            
-            # move to origin
-            cmds.setAttr("%s.translateX" % selection, 0);
-            cmds.setAttr("%s.translateY" % selection, 0);
-            cmds.setAttr("%s.translateZ" % selection, 0);
+
+            if move:
+                # store object position
+                x_value = cmds.getAttr("%s.translateX" % selection)
+                y_value = cmds.getAttr("%s.translateY" % selection)
+                z_value = cmds.getAttr("%s.translateZ" % selection)
+                
+                # move to origin
+                cmds.setAttr("%s.translateX" % selection, 0);
+                cmds.setAttr("%s.translateY" % selection, 0);
+                cmds.setAttr("%s.translateZ" % selection, 0);
             
             # freeze transforms
-            cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=2)
+            if freeze:
+                cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=2)
             
             if '|' in selection:
                 selection = selection.partition('|')[2]
@@ -149,9 +165,10 @@ class Exporter(QtWidgets.QDialog):
             self.ui.output.setPlainText(text + "Exporting: " + selection + ext + "\n")
             
             # move back to origional position
-            cmds.setAttr("%s.translateX" % selection, x_value);
-            cmds.setAttr("%s.translateY" % selection, y_value);
-            cmds.setAttr("%s.translateZ" % selection, z_value);
+            if move:
+                cmds.setAttr("%s.translateX" % selection, x_value);
+                cmds.setAttr("%s.translateY" % selection, y_value);
+                cmds.setAttr("%s.translateZ" % selection, z_value);
             
         text = self.ui.output.toPlainText()
         self.ui.output.setPlainText(text + "Finished!")
